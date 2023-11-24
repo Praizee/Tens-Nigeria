642771625eed34946e891194180696fce97a1614
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-24 15:52:34 +0100

    Added flowbite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -33,6 +34,6 @@ export default {
       black: '900',
     }
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('flowbite/plugin')],
 
 }
